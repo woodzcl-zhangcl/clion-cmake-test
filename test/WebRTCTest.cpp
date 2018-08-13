e61a0d0caccab90952ee25b9dc2f18eec10e8017
zhangcl <791398105@qq.com>
2018-08-13 22:15:33 +0800

    Add WebRTC to project, but no lib to support for mac os

---
+++ test/WebRTCTest.cpp
@@ -0,0 +1,24 @@
+// Copyright (c) 2012-2018 The woodzcl Open Source Project
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+//
+// Created by woodzcl on 2018/8/13.
+//
+
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+
+#include <iostream>
+#include <unistd.h>
+
+#include "api/test/fakeconstraints.h"
+
+using namespace webrtc;
+
+TEST_CASE("webrtc", "[WebRTC]") {
+    FakeConstraints fc;
+
+}
+
