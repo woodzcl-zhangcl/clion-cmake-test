0f14e06da392851c52886c93564576352f4e2656
zhangcl <791398105@qq.com>
2018-08-22 19:34:09 +0800

    micro changing

--- test/ffmpegTest.cpp
+++ test/ffmpegTest.cpp
@@ -168,7 +168,7 @@ TEST_CASE("ffmpeg", "[FFMPEG]") {
  */
 
 
-TEST_CASE("stream", "[FFMPEG]") {
+TEST_CASE("stream out", "[FFMPEG]") {
     if (0 == TEST_2)
         return;
 
@@ -237,9 +237,8 @@ TEST_CASE("stream", "[FFMPEG]") {
         //Copy the settings of AVCodecContext
         out_stream->codecpar = in_stream->codecpar;
         out_stream->codecpar->codec_tag = 0;
-        if (ofmt_ctx->oformat->flags & AVFMT_GLOBALHEADER) {
-//            out_stream->codec->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
-        }
+        if (ofmt_ctx->oformat->flags & AVFMT_GLOBALHEADER)
+            out_stream->codec->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
     }
     //Dump Format------------------
     av_dump_format(ofmt_ctx, 0, out_filename, 1);
@@ -314,6 +313,148 @@ TEST_CASE("stream", "[FFMPEG]") {
         av_packet_unref(&pkt);
 
     }
+    //Write file trailer
+    av_write_trailer(ofmt_ctx);
+
+    end:
+
+    avformat_close_input(&ifmt_ctx);
+    /* close output */
+    if (ofmt_ctx && !(ofmt->flags & AVFMT_NOFILE))
+        avio_close(ofmt_ctx->pb);
+    avformat_free_context(ofmt_ctx);
+    if (ret < 0 && ret != AVERROR_EOF) {
+        printf("Error occurred.\n");
+        return;
+    }
+}
+
+//'1': Use H.264 Bitstream Filter
+#define USE_H264BSF 0
+
+TEST_CASE("stream in", "[FFMPEG]") {
+    AVOutputFormat *ofmt = nullptr;
+    //Input AVFormatContext and Output AVFormatContext
+    AVFormatContext *ifmt_ctx = nullptr, *ofmt_ctx = nullptr;
+    AVPacket pkt;
+    const char *in_filename, *out_filename;
+    int ret, i;
+    int videoindex = -1;
+    int frame_index = 0;
+    in_filename = "rtmp://live.hkstv.hk.lxdns.com/live/hks";
+    //in_filename  = "rtp://233.233.233.233:6666";
+    //out_filename = "receive.ts";
+    //out_filename = "receive.mkv";
+    out_filename = "receive.flv";
+
+    //Network
+    avformat_network_init();
+    //Input
+    if ((ret = avformat_open_input(&ifmt_ctx, in_filename, 0, 0)) < 0) {
+        printf("Could not open input file.");
+        goto end;
+    }
+    if ((ret = avformat_find_stream_info(ifmt_ctx, 0)) < 0) {
+        printf("Failed to retrieve input stream information");
+        goto end;
+    }
+
+    for (i = 0; i < ifmt_ctx->nb_streams; i++)
+        if (ifmt_ctx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
+            videoindex = i;
+            break;
+        }
+
+    av_dump_format(ifmt_ctx, 0, in_filename, 0);
+
+    //Output
+    avformat_alloc_output_context2(&ofmt_ctx, nullptr, nullptr, out_filename); //RTMP
+
+    if (!ofmt_ctx) {
+        printf("Could not create output context\n");
+        ret = AVERROR_UNKNOWN;
+        goto end;
+    }
+    ofmt = ofmt_ctx->oformat;
+    for (i = 0; i < ifmt_ctx->nb_streams; i++) {
+        //Create output AVStream according to input AVStream
+        AVStream *in_stream = ifmt_ctx->streams[i];
+        AVCodec *codec = avcodec_find_decoder(in_stream->codecpar->codec_id);
+        AVStream *out_stream = avformat_new_stream(ofmt_ctx, codec);
+        if (!out_stream) {
+            printf("Failed allocating output stream\n");
+            ret = AVERROR_UNKNOWN;
+            goto end;
+        }
+        //Copy the settings of AVCodecContext
+        out_stream->codecpar = in_stream->codecpar;
+        out_stream->codecpar->codec_tag = 0;
+        if (ofmt_ctx->oformat->flags & AVFMT_GLOBALHEADER)
+            out_stream->codec->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+    }
+    //Dump Format------------------
+    av_dump_format(ofmt_ctx, 0, out_filename, 1);
+    //Open output URL
+    if (!(ofmt->flags & AVFMT_NOFILE)) {
+        ret = avio_open(&ofmt_ctx->pb, out_filename, AVIO_FLAG_WRITE);
+        if (ret < 0) {
+            printf("Could not open output URL '%s'", out_filename);
+            goto end;
+        }
+    }
+    //Write file header
+    ret = avformat_write_header(ofmt_ctx, nullptr);
+    if (ret < 0) {
+        printf("Error occurred when opening output URL\n");
+        goto end;
+    }
+
+#if USE_H264BSF
+    AVBitStreamFilterContext* h264bsfc =  av_bitstream_filter_init("h264_mp4toannexb");
+#endif
+
+    while (1) {
+        AVStream *in_stream, *out_stream;
+        //Get an AVPacket
+        ret = av_read_frame(ifmt_ctx, &pkt);
+        if (ret < 0)
+            break;
+
+        in_stream = ifmt_ctx->streams[pkt.stream_index];
+        out_stream = ofmt_ctx->streams[pkt.stream_index];
+        /* copy packet */
+        //Convert PTS/DTS
+        pkt.pts = av_rescale_q_rnd(pkt.pts, in_stream->time_base, out_stream->time_base,
+                                   (AVRounding) (AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX));
+        pkt.dts = av_rescale_q_rnd(pkt.dts, in_stream->time_base, out_stream->time_base,
+                                   (AVRounding) (AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX));
+        pkt.duration = av_rescale_q(pkt.duration, in_stream->time_base, out_stream->time_base);
+        pkt.pos = -1;
+        //Print to Screen
+        if (pkt.stream_index == videoindex) {
+            printf("Receive %8d video frames from input URL\n", frame_index);
+            frame_index++;
+
+#if USE_H264BSF
+            av_bitstream_filter_filter(h264bsfc, in_stream->codec, nullptr, &pkt.data, &pkt.size, pkt.data, pkt.size, 0);
+#endif
+        }
+        //ret = av_write_frame(ofmt_ctx, &pkt);
+        ret = av_interleaved_write_frame(ofmt_ctx, &pkt);
+
+        if (ret < 0) {
+            printf("Error muxing packet\n");
+            break;
+        }
+
+        av_packet_unref(&pkt);
+
+    }
+
+#if USE_H264BSF
+    av_bitstream_filter_close(h264bsfc);
+#endif
+
     //Write file trailer
     av_write_trailer(ofmt_ctx);
 
